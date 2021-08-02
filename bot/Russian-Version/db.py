90dc36f90bdb0352175f333b7b70e11778fc1bd3
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-08-02 20:51:07 +0300

    Update db.py

--- bot/Russian-Version/db.py
+++ bot/Russian-Version/db.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import random
 
 import sqlite3
@@ -14,21 +16,16 @@ def generator_id():
 
 
 def ensure_connection(func):
-
     def decorator(*args, **kwargs):
         with sqlite3.connect('base.db') as conn:
             result = func(conn, *args, **kwargs)
-
         return result
-
     return decorator
 
 
 @ensure_connection
 def init_db(conn, force: bool = False):
-
     c = conn.cursor()
-
     if force:
         c.execute("DROP TABLE IF EXISTS users")
 
@@ -63,27 +60,22 @@ def init_db(conn, force: bool = False):
 @ensure_connection
 def add_user(conn, first_name: str, last_name: str, date_reg: str, user_id):
     c = conn.cursor()
-
     c.execute("INSERT INTO users (first_name, last_name, date_reg, user_id) VALUES (?, ?, ?, ?)",
               (first_name, last_name, date_reg, user_id))
-
     conn.commit()
 
 
 @ensure_connection
 def add_answer(conn, date: str, message_id, text: str):
     c = conn.cursor()
-
     c.execute("INSERT INTO answers (date, message_id, text) VALUES (?, ?, ?)",
               (date, message_id, text))
-
     conn.commit()
 
 
 @ensure_connection
 def add_message(conn, first_name: str, last_name: str, date: str, message: str, user_id):
     id_database = list(return_messages_id())
-
     while True:
         message_id = generator_id()
         if message_id not in id_database:
@@ -97,86 +89,62 @@ def add_message(conn, first_name: str, last_name: str, date: str, message: str, 
 @ensure_connection
 def return_users_id(conn):
     c = conn.cursor()
-
     c.execute("SELECT user_id FROM users")
-
     all_results = c.fetchall()
-
     return all_results
 
 
 @ensure_connection
 def return_user_id(conn, message_id):
     c = conn.cursor()
-
     c.execute("SELECT user_id FROM requests WHERE message_id = ?", (message_id,))
-
     all_results = c.fetchone()
-
     return all_results
 
 
 @ensure_connection
 def return_message(conn, user_id):
     c = conn.cursor()
-
     c.execute("SELECT message FROM requests WHERE user_id = ?", (user_id,))
-
     all_results = c.fetchone()
-
     return all_results
 
 
 @ensure_connection
 def return_message_id(conn, message: str):
     c = conn.cursor()
-
     c.execute("SELECT message_id FROM requests WHERE message = ?", (message,))
-
     all_results = c.fetchone()
-
     return all_results
 
 
 @ensure_connection
 def return_messages_id(conn):
     c = conn.cursor()
-
     c.execute("SELECT message_id FROM requests")
-
     all_results = c.fetchall()
-
     return all_results
 
 
 @ensure_connection
 def return_requests(conn):
     c = conn.cursor()
-
     c.execute("SELECT COUNT(message_id) FROM requests")
-
     all_results = c.fetchall()
-
     return all_results
 
 
 @ensure_connection
 def return_users(conn):
     c = conn.cursor()
-
     c.execute("SELECT COUNT(user_id) FROM users")
-
     all_results = c.fetchall()
-
     return all_results
 
 
 @ensure_connection
 def return_answers(conn):
     c = conn.cursor()
-
     c.execute("SELECT COUNT(id) FROM answers")
-
     all_results = c.fetchall()
-
     return all_results
